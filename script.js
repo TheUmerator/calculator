09bd1dffeb3359bf398edde04bd07fe77fc50631
UmerHP <54628763+TheUmerator@users.noreply.github.com>
2023-08-20 14:35:23 +0500

    Add barebones operate function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let firstNum,secondNum,operator;
+
 function add(a,b) {
     return a+b;
 }
@@ -9,4 +11,15 @@ function multiply(a,b) {
 }
 function divide(a,b) {
     return a/b;
+}
+/*
+    For now: Lets create a function that
+    takes what the user inputed and split it into
+    it's respective values
+*/
+function splitUp(){
+    
+}
+function operate(firstNum,operator,secondNum){
+
 }
