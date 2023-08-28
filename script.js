e49b4c5b5a10ea7cc8e5c4984bf0918a11ffbf00
umerHP <54628763+TheUmerator@users.noreply.github.com>
2023-08-28 09:28:54 +0500

    Connect button functionality to keypad press

--- script.js
+++ script.js
@@ -38,15 +38,15 @@ function operate(userInput) {
 //INPUTS: PRESSED KEY(STRING), STRING
 function displayToScreen(key, userInput) {
     const screen = document.querySelector('.screen');
-    if (key == 'Backspace') {
+    if (key == 'Backspace'||key == '~') {
         key = '';
         // userInput = userInput.slice(0, userInput.length - 1)
     }
-    if (key == 'c') {
+    if (key == 'c'||key == 'clear') {
         key = '';
     }
 
-    if (key == 'Enter')
+    if (key == 'Enter'||key == '=')
         key = '';
 
     userInput = screen.textContent = userInput + key;
@@ -67,6 +67,9 @@ addEventListener('keydown', (e) => {
     }
     if (e.key == 'c') {
         inputString = '';
+        inputObj.firstNum='';
+        inputObj.secondNum='';
+        inputObj.operator='';
         inputString = displayToScreen(e.key, inputString);
     }
     if (e.location == 3) //3 is the code for the keypad location
@@ -96,3 +99,70 @@ addEventListener('keydown', (e) => {
     }
 });
 
+const calcButtons = document.querySelectorAll('button');
+
+calcButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        
+        
+        if (button.textContent == 'clear') {
+            inputString = '';
+            inputObj.firstNum='';
+            inputObj.secondNum='';
+            inputObj.operator='';
+            inputString = displayToScreen(button.textContent, inputString);
+        }
+        
+        if (button.textContent == '~') {
+            inputString = inputString.slice(0, inputString.length - 1)
+            inputString = displayToScreen(button.textContent, inputString);
+        }
+        if (button.textContent == 'clear') {
+            inputString = '';
+            inputString = displayToScreen(button.textContent, inputString);
+        }
+
+
+            inputString = displayToScreen(button.textContent, inputString);
+    
+            for (i = 0; i <= inputString.length; i++) {
+                if (inputString[i] == '+' || inputString[i] == '-' ||
+                    inputString[i] == '*' || inputString[i] == '/') {
+                    inputObj.firstNum = inputString.substring(0, i);
+                    inputObj.secondNum = inputString.substring(i + 1, inputString.length);
+                    inputObj.operator = inputString[i];
+                }
+            }
+    
+    
+            if ((inputObj.firstNum != '') && (inputObj.secondNum != '') && (inputObj.operator != '')) {
+                console.log("BUTTON READY TO OPERATE");
+                if (button.textContent == '=') {
+                    cal = operate(inputObj);
+                    inputString = displayToScreen(button.textContent, cal);
+                }
+                if (button.textContent == '+' || button.textContent == '-' || button.textContent == '*' || button.textContent == '/') {
+                    cal = operate(inputObj);
+                    inputString = displayToScreen(button.textContent, cal);
+                }
+            }
+
+        // console.log(button.textContent)
+        //displayToScreen(button.textContent,'');
+
+    });
+})
+
+/*
+-want button press and keypad press to append to the same variable
+ inputString
+
+
+
+let cal;
+let inputObj = {
+    firstNum: '',
+    secondNum: '',
+    operator: '',
+};
+*/
